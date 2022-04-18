3efc443e974c861aef837808c87ad47dab6c0df8
dorapocket <993549790@qq.com>
2022-04-18 12:11:50 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,17 @@
 declare module 'saltdog' {
-    export const command: (str: string) => any;
+    /**
+     * @param command command to register to context.
+     * @param execFn  Function execute when `command` has been invoked
+     * @description Register a global command to Saltdog
+     */
+    export const registerCommand: (command: string, execFn: typeof Function) => any;
+
+    /**
+     * @returns ISaltDogPDFView | null
+     * @description `PDFView` instence if current tab is a pdf tab, else return `null`.
+     */
+    export const getCurrentPDFView: () => ISaltDogPDFView | null;
+}
+interface ISaltDogPDFView {
+    id: string;
 }
