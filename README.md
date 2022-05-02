7e2877b3f92baebd223c5ecc3ed5d31207b67c49
Dorapocket <993549790@qq.com>
2022-05-02 13:11:07 +0800

    feat: commands

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 This package is used to provide type definitation for saltdog plugin developers.
 
 Implements are in (here)[https://github.com/SaltDogProject/SaltDog/tree/main/src/pluginHost/api]
+
+## Debug
+
+In this directory:
+```
+yarn link
+```
+
+In target directory using 'saltdog'
+```
+yarn link saltdog
+```
--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,22 @@
 declare module 'saltdog' {
     /**
-     * @param command command to register to context.
-     * @param execFn  Function execute when `command` has been invoked
-     * @description Register a global command to Saltdog
+     * @description SaltDog Commands
      */
-    export const registerCommand: (command: string, execFn: typeof Function) => any;
-
+    interface commands {
+        /**
+         * @param command command to register to context.
+         * @param execFn  Function execute when `command` has been invoked
+         * @description Register a global command to Saltdog
+         */
+        registerCommand: (command: string, execFn: (...args: any)=>any) => any;
+        /**
+         * @param command command to run.
+         * @param args  Params in `registerCommand`'s execFn.
+         * @description Execute a global command in Saltdog.
+         */
+        executeCommand:(command:string, ...args:any)=>any;
+    }
+    export const commands:commands;
     /**
      * @returns ISaltDogPDFView | null
      * @description `PDFView` instence if current tab is a pdf tab, else return `null`.
