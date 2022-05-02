c1dc9c382f7db1f4036600729668f8898bf48c8c
dorapocket <993549790@qq.com>
2022-05-02 23:04:01 +0800

    add reader

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,34 @@
 declare module 'saltdog' {
-    /**
-     * @description SaltDog Commands
-     */
     interface commands {
         /**
          * @param command command to register to context.
          * @param execFn  Function execute when `command` has been invoked
          * @description Register a global command to Saltdog
          */
-        registerCommand: (command: string, execFn: (...args: any)=>any) => any;
+        registerCommand: (command: string, execFn: (...args: any) => any) => any;
         /**
          * @param command command to run.
          * @param args  Params in `registerCommand`'s execFn.
          * @description Execute a global command in Saltdog.
          */
-        executeCommand:(command:string, ...args:any)=>any;
+        executeCommand: (command: string, ...args: any) => any;
     }
-    export const commands:commands;
+    /**
+     * @description SaltDog Commands
+     */
+    export const commands: commands;
+
+    interface reader {
+        /**
+         * @param fn Callback function when text select on reader.
+         * @description Listen text select events on all reader.
+         */
+        onTextSelect: (fn: (text: string) => any) => any;
+    }
+    /**
+     * @description Saltdog reader Object.
+     */
+    export const reader: reader;
     /**
      * @returns ISaltDogPDFView | null
      * @description `PDFView` instence if current tab is a pdf tab, else return `null`.
