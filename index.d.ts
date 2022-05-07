6669b36e530484bb3973451f7e897e61fab3151c
dorapocket <993549790@qq.com>
2022-05-07 13:49:02 +0800

    feat: getSidebarView judge

--- index.d.ts
+++ index.d.ts
@@ -33,7 +33,44 @@ declare module 'saltdog' {
      * @returns ISaltDogPDFView | null
      * @description `PDFView` instence if current tab is a pdf tab, else return `null`.
      */
-    export const getCurrentPDFView: () => ISaltDogPDFView | null;
+    interface sidebar {
+        /**
+         * @returns boolean
+         * @description Check if `viewName` is open.
+         */
+        isOpen: () => boolean;
+
+        /**
+         * @param channel Channel to send.
+         * @param `...args` Args to send.
+         * @description Send a message to `viewName`'s sidebar.
+         */
+        send: (channel: string, ...args: any) => void;
+
+        /**
+         * @param channel Channel to listen.
+         * @param callback Callback function to execute when `channel` has been received.
+         * @description Listen `channel` on `viewName`'s sidebar.
+         */
+        on: (channel: string, callback: (...args: any) => any) => void;
+
+        /**
+         * @param action `open` or `close` action.
+         * @param callback Callback function to execute when `viewName` has been opened or closed.
+         * @description Listen `viewName`'s visibility change.
+         */
+        onVisibilityChange: (action: 'open' | 'close', callback: (...args: any) => any) => void;
+    }
+
+    export const sidebar: {
+        /**
+         * @param viewName viewName.
+         * @returns sidebar | null if not registered.
+         * @description Get `viewName` to invoke sidebar methods.
+         */
+        getSidebarView: (viewName: string) => sidebar | null;
+    };
+    // export const getCurrentPDFView: () => ISaltDogPDFView | null;
 }
 interface ISaltDogPDFView {
     id: string;
